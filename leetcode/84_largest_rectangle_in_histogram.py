4db528687b8350d7a30bcd2f34bc4022fe42a067
Lance Wong <lwong180@gmail.com>
2020-01-22 00:36:07 -0800

    optimized solution to largest rectangle in histogram problem

--- leetcode/84_largest_rectangle_in_histogram.py
+++ leetcode/84_largest_rectangle_in_histogram.py
@@ -4,7 +4,7 @@ Given n non-negative integers representing the histogram's bar height where the 
 Problem link: https://leetcode.com/problems/largest-rectangle-in-histogram/description/
 """
 
-def largest_rectangle_area_1:
+def largest_rectangle_area_slow:
     """
     Nested loop strategy: go through each bar width possibility, setting the
     shortest rectangle's height as the height for calculating area. The result
@@ -19,3 +19,27 @@ def largest_rectangle_area_1:
             h = min(h, heights[j])
             largest_area = max(largest_area, h * (j - i + 1))
     return largest_area
+
+def largest_rectangle_area_fast:
+    """
+    We can improve on the first solution by cutting the number of times we
+    iterate the 'heights' input. The optimization idea uses the fact that
+    rectangles are surrounded by shorter bars or the start and end of the
+    array. The strategy is to keep track of elements by storing the index in an
+    array (that way we can keep track of where an observed rectangle starts and
+    ends, as well as keep track of the heights). Then when we encounter a value
+    that ends a rectangle, we can look in the stack and work backwards from
+    there.
+
+    Time complexity: O(n) because it passes array items at most twice.
+    Space complexity: O(n) - space used increases linearly with size of input.
+    """
+    largest_area, prev_encountered = 0, []
+    for i, end in enumerate(heights + [0]):
+        while prev_encountered and heights[prev_encountered[-1]] >= end:
+            height = heights[prev_encountered.pop()]
+            width = i if not prev_encountered else i - prev_encountered[-1] - 1
+            largest_area = max(largest_area, height * width)
+        prev_encountered.append(i)
+    return largest_area
+    
