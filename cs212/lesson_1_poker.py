81b32060a97c7dea7ffb8996af19eb30b447d34d
Lance Wong <lwong180@gmail.com>
2020-02-10 14:20:14 -0800

    function ideas, tests

--- cs212/lesson_1_poker.py
+++ cs212/lesson_1_poker.py
@@ -10,6 +10,19 @@ hand rank concepts: n-kind, straight, flush
 def poker(hands):
     """
     Return the best hand: poker([hand,...]) => hand
-    TODO: define max function that returns best hand
+    TODO: define max key to calculate value of hands
     """
-    return max(hands)
+    return max(hands, key=hand_rank)
+
+def hand_rank(hand):
+    """Returns rank for a hand"""
+    return None
+
+def test():
+    """Test cases for the functions in a poker program"""
+    sf = '6C 7C 8C 9C TC'.split()
+    fk = '9D 9C 9H 9S 7D'.split()
+    fh = 'TD TC TH 7H 7D'.split()
+    assert(poker([sf, fk, fh])) == sf
+    assert(poker([fk, fh])) == fk
+    assert(poker([fh, fh])) == fh
