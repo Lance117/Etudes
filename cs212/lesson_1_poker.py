93e79962a83e440712ee1394943c1096fba2a6b2
Lance Wong <lwong180@gmail.com>
2020-02-11 11:30:31 -0800

    straight, n-kind, flush, twopair functions, and tests

--- cs212/lesson_1_poker.py
+++ cs212/lesson_1_poker.py
@@ -51,22 +51,61 @@ def card_ranks(cards):
     ranks.sort(reverse=True)
     return ranks
 
+def straight(ranks):
+    """Return True if the ordered ranks form a 5-card straight."""
+    return max(ranks) - min(ranks) == 4 and len(set(ranks)) == 5
+
+def flush(hand):
+    """Return True if all the cards have the same suit"""
+    return len(set([s for r,s in hand])) == 1
+
+def kind(n, ranks):
+    """
+    Return the first rank that this hand has exactly n of.
+    Return None if there is no n-of-a-kind in the hand.
+    """
+    for rank in ranks:
+        if ranks.count(rank) == n:
+            return rank
+    return None
+
+def two_pair(ranks):
+    """
+    If there are two pair, return the two ranks as a
+    tuple: (highest, lowest); otherwise return None.
+    """
+    pair, lowpair = kind(2, ranks), kind(2, list(reversed(ranks)))
+    return (pair, lowpair) if pair and pair != lowpair else None
+
 def test():
     """Test cases for the functions in a poker program"""
     sf = '6C 7C 8C 9C TC'.split()
     fk = '9D 9C 9H 9S 7D'.split()
     fh = 'TD TC TH 7H 7D'.split()
+    tp = '5S 5D 9H 9C 6S'.split()
+    fkranks = card_ranks(fk)
+    tpranks = card_ranks(tp)
     assert(card_ranks(sf)) == [10, 9, 8, 7, 6]
     assert(card_ranks(fk)) == [9, 9, 9, 9, 7]
     assert(card_ranks(fh)) == [10, 10, 10, 7, 7]
-    assert(poker([sf, fk, fh])) == sf
-    assert(poker([fk, fh])) == fk
-    assert(poker([fh, fh])) == fh
-    assert(poker([fh])) == fh
-    assert(poker([sf] + [fh] * 99)) == sf
-    assert(hand_rank(sf)) == (8, 10)
-    assert(hand_rank(fk)) == (7, 9, 7)
-    assert(hand_rank(fh)) == (6, 10, 7)
+    assert(kind(4, fkranks)) == 9
+    assert(kind(3, fkranks)) == None
+    assert(kind(2, fkranks)) == None
+    assert(kind(1, fkranks)) == 7
+    assert(straight([9, 8, 7, 6, 5])) == True
+    assert(straight([9, 8, 7, 6, 4])) == False
+    assert(flush(sf)) == True
+    assert(flush(fk)) == False
+    assert(two_pair(fkranks)) == None
+    assert(two_pair(tpranks)) == (9, 5)
+    # assert(hand_rank(sf)) == (8, 10)
+    # assert(hand_rank(fk)) == (7, 9, 7)
+    # assert(hand_rank(fh)) == (6, 10, 7)
+    # assert(poker([sf, fk, fh])) == sf
+    # assert(poker([fk, fh])) == fk
+    # assert(poker([fh, fh])) == fh
+    # assert(poker([fh])) == fh
+    # assert(poker([sf] + [fh] * 99)) == sf
     return 'tests pass'
 
 print(test())
