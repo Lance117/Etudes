1095c872d01eb69b6205c830bb0878fc50b00f6e
Lance Wong <lwong180@gmail.com>
2020-02-12 23:24:01 -0800

    modify poker function to handle ties

--- cs212/lesson_1_poker.py
+++ cs212/lesson_1_poker.py
@@ -10,20 +10,24 @@ hand rank concepts: n-kind, straight, flush, two-pair
 def poker(hands):
     """
     Return the best hand: poker([hand,...]) => hand
-    TODO: define max key to calculate value of hands
     """
-    return max(hands, key=hand_rank)
+    return allmax(hands, key=hand_rank)
+
+def allmax(iterable, key=None):
+    """Return a list of all items equal to the max of the iterable."""
+    res, maxval = [], None
+    key = key or (lambda x: x)
+    for x in iterable:
+        cur_val = key(x)
+        if not res or cur_val > maxval:
+            res, maxval = [x], cur_val
+        elif cur_val == maxval:
+            res.append(x)
+    return res
 
 def hand_rank(hand):
     """
     Returns rank for a hand. Idea is to use tuple comparison to evaluate hand rank.
-    TODO: functions to determine hand type
-
-    straight(ranks): True if hand is straight
-    flush(hand): True if hand is flush
-    kind(n, ranks): returns first rank that hand has n of
-    two_pair(ranks): if two pair, returns a tuple
-    card_ranks(hand): returns ORDERED tuple of ranks in a hand (highest to lowest)
     """
     ranks = card_ranks(hand)
     if straight(ranks) and flush(hand):
@@ -80,6 +84,7 @@ def two_pair(ranks):
 def test():
     """Test cases for the functions in a poker program"""
     sf = '6C 7C 8C 9C TC'.split()
+    sf2 = '6D 7D 8D 9D TD'.split()
     fk = '9D 9C 9H 9S 7D'.split()
     fh = 'TD TC TH 7H 7D'.split()
     tp = '5S 5D 9H 9C 6S'.split()
@@ -103,14 +108,14 @@ def test():
     assert(flush(fk)) == False
     assert(two_pair(fkranks)) == None
     assert(two_pair(tpranks)) == (9, 5)
-    # assert(hand_rank(sf)) == (8, 10)
-    # assert(hand_rank(fk)) == (7, 9, 7)
-    # assert(hand_rank(fh)) == (6, 10, 7)
-    # assert(poker([sf, fk, fh])) == sf
-    # assert(poker([fk, fh])) == fk
-    # assert(poker([fh, fh])) == fh
-    # assert(poker([fh])) == fh
-    # assert(poker([sf] + [fh] * 99)) == sf
+    assert(hand_rank(sf)) == (8, 10)
+    assert(hand_rank(fk)) == (7, 9, 7)
+    assert(hand_rank(fh)) == (6, 10, 7)
+    assert(poker([sf, fk, fh])) == [sf]
+    assert(poker([fk, fh])) == [fk]
+    assert(poker([sf, sf2])) == [sf, sf2]
+    assert(poker([fh])) == [fh]
+    assert(poker([sf] + [fh] * 99)) == [sf]
     return 'tests pass'
 
 print(test())
