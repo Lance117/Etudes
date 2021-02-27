ed17771843bac690abcf4c3614b6f53e082cda5a
Lance Wong <lwong180@gmail.com>
2021-02-27 15:22:40 -0800

    backtrack permutations optimized

--- leetcode/backtracking/46_permutations.py
+++ leetcode/backtracking/46_permutations.py
@@ -1,15 +1,14 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def backtrack(perm):
-            if len(perm) == len(nums):
-                res.append(perm[:])
+        def backtrack(i):
+            if i == len(nums):
+                res.append(nums[:])
                 return
-            for num in nums:
-                if num not in perm:
-                    perm.append(num)
-                    backtrack(perm)
-                    perm.pop()              
+            for j in range(i, len(nums)):
+                nums[i], nums[j] = nums[j], nums[i]
+                backtrack(i + 1)
+                nums[i], nums[j] = nums[j], nums[i]
         
         res = []
-        backtrack([])
+        backtrack(0)
         return res
