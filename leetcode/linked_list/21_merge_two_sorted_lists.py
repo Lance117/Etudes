3acbc41df6f6de5fe3e9a09fc68605ea2e9d8c6a
Lance Wong <lwong180@gmail.com>
2021-04-27 19:56:49 -0700

    merge 2 sorted lists recursive

--- leetcode/linked_list/21_merge_two_sorted_lists.py
+++ leetcode/linked_list/21_merge_two_sorted_lists.py
@@ -20,3 +20,13 @@ class Solution:
             tail = tail.next
         tail.next = l1 or l2
         return sentinel.next
+
+    def mergeTwoLists2(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not (l1 and l2):
+            return l1 or l2
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
