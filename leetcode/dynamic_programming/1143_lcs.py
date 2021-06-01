cdfa9f4cdfa1349212cd143c3f0c594c6c9123f0
Lance Wong <lwong180@gmail.com>
2021-05-31 22:29:03 -0700

    lcs recursive solution

--- leetcode/dynamic_programming/1143_lcs.py
+++ leetcode/dynamic_programming/1143_lcs.py
@@ -9,3 +9,17 @@ class Solution:
                 else:
                     cache[i][j] = max(cache[i - 1][j], cache[i][j - 1])
         return cache[-1][-1]
+
+    def longestCommonSubsequence2(self, text1: str, text2: str) -> int:
+        cache = {}
+        def dp(i, j):
+            if i < 0 or j < 0:
+                return 0
+            if (i, j) in cache:
+                return cache[(i, j)]
+            if text1[i] == text2[j]:
+                return dp(i - 1, j - 1) + 1
+            res = max(dp(i - 1, j), dp(i, j - 1))
+            cache[(i, j)] = res
+            return res
+        return dp(len(text1) - 1, len(text2) - 1)
